86c1cdcb5c60bc44bb4f2f6bd3a01860db7f1627
Rei Sugiura <reibomaru@g.ecc.u-tokyo.ac.jp>
2020-04-12 12:40:14 +0900

    ユーザーページを作成した

--- main.go
+++ main.go
@@ -23,8 +23,9 @@ type Knowledges struct {
 	Content string
 }
 
-const lenPathKnowledges = len("/admin/knowledges/")
+const lenPathAdminKnowledges = len("/admin/knowledges/")
 const lenPathDelete = len("/admin/delete/")
+const lenPathKnowledges = len("/knowledges/")
 
 var env = make(map[string]string)
 
@@ -37,9 +38,8 @@ func init() {
 	env["sqlEnv"] = string(sqlenv)
 }
 
-func knowledgesHandler(w http.ResponseWriter, r *http.Request) {
-
-	suffix := r.URL.Path[lenPathKnowledges:]
+func adminKnowledgesHandler(w http.ResponseWriter, r *http.Request) {
+	suffix := r.URL.Path[lenPathAdminKnowledges:]
 	db, err := sql.Open("mysql", env["sqlEnv"])
 	if err != nil {
 		panic(err.Error())
@@ -130,12 +130,65 @@ func deleteHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/admin/knowledges/", http.StatusFound)
 }
 
+func knowledgesHandler(w http.ResponseWriter, r *http.Request) {
+	suffix := r.URL.Path[lenPathKnowledges:]
+	db, err := sql.Open("mysql", env["sqlEnv"])
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	if suffix != "" {
+		var editPage Knowledges
+		var id int
+		id, _ = strconv.Atoi(suffix)
+		err := db.QueryRow("SELECT id, title, content FROM knowledges WHERE id = ?", id).Scan(&editPage.Id, &editPage.Title, &editPage.Content)
+		switch {
+		case err == sql.ErrNoRows:
+			log.Println("レコードが存在しません")
+			http.NotFound(w, r)
+		case err != nil:
+			panic(err.Error())
+		default:
+			t := template.Must(template.ParseFiles("template/user_details.html"))
+			err = t.Execute(w, editPage)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+		}
+	} else {
+		rows, err := db.Query("SELECT id, title FROM knowledges")
+		if err != nil {
+			panic(err.Error())
+		}
+		defer rows.Close()
+		var indexPages []IndexPage
+
+		for rows.Next() {
+			var indexPage IndexPage
+			err := rows.Scan(&indexPage.Id, &indexPage.Title)
+			if err != nil {
+				panic(err.Error())
+			}
+			indexPages = append(indexPages, indexPage)
+		}
+
+		t := template.Must(template.ParseFiles("template/user_knowledges.html"))
+		err = t.Execute(w, indexPages)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	}
+
+}
+
 func main() {
 	dir, _ := os.Getwd()
-	http.HandleFunc("/admin/knowledges/", knowledgesHandler)
+	http.HandleFunc("/admin/knowledges/", adminKnowledgesHandler)
+	http.Handle("/admin/new/", http.StripPrefix("/admin/new/", http.FileServer(http.Dir(dir+"/static/admin_new/"))))
 	http.HandleFunc("/admin/save/", saveHandler)
 	http.HandleFunc("/admin/delete/", deleteHandler)
+	http.HandleFunc("/knowledges/", knowledgesHandler)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir(dir+"/static/"))))
-	http.Handle("/admin/new/", http.StripPrefix("/admin/new/", http.FileServer(http.Dir(dir+"/static/admin_new/"))))
 	http.ListenAndServe(":3000", nil)
 }

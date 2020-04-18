ff6a98c1bf94cd8641a997966035f574a32fe0b0
Rei Sugiura <reibomaru@g.ecc.u-tokyo.ac.jp>
2020-04-18 10:58:50 +0900

    404ページにフッターを入れた

--- handlers/statusNotFound.go
+++ handlers/statusNotFound.go
@@ -12,7 +12,7 @@ func StatusNotFoundHandler(w http.ResponseWriter, r *http.Request) {
 	if auth, ok := session.Values["authenticated"].(bool); ok && auth {
 		header.IsLogin = true
 	}
-	t := template.Must(template.ParseFiles("template/404.html", "template/_header.html"))
+	t := template.Must(template.ParseFiles("template/404.html", "template/_header.html", "template/_footer.html"))
 	if err := t.Execute(w, struct {
 		Header Header
 	}{

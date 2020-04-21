161809b2b6aae98cbfda65d2c04def6bf141c842
Rei Sugiura <reibomaru@g.ecc.u-tokyo.ac.jp>
2020-04-21 18:50:54 +0900

    コメントアウトを削除

--- main.go
+++ main.go
@@ -50,9 +50,4 @@ func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir(dir+"/static/"))))
 	http.Handle("/node_modules/", http.StripPrefix("/node_modules/", http.FileServer(http.Dir(dir+"/node_modules/"))))
 	http.ListenAndServe(":3000", nil)
-	// l, err := net.Listen("tcp", "127.0.0.1:9000")
-	// if err != nil {
-	// 	return
-	// }
-	// fcgi.Serve(l, nil)
 }

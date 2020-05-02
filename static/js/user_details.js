32945e2179d6668a05fad83c420331711724337d
今村友哉 <imatomo1234imatomo@gmail.com>
2020-05-02 14:47:01 +0900

    アイコンの大きさの修正

--- static/js/user_details.js
+++ static/js/user_details.js
@@ -6,12 +6,12 @@ const likes_inline = document.getElementById('likes_inline')
 const likes_baloon = document.getElementById('likes_baloon')
 const knowledge_id = document.getElementById('knowledge_id').value
 // SNSボタンを追加するエリア
-var snsArea = document.getElementById('sns-area');
-var title = document.getElementById('title').innerHTML;
+let snsArea = document.getElementById('sns-area');
+let title = document.getElementById('title').innerHTML;
 
 // シェア時に使用する値
-var shareUrl = location.href; // 現在のページURLを使用する場合 location.href;
-var shareText = title+'\n#駆け出しエンジニアと繋がりたい\n#プログラミング初心者'; // 現在のページタイトルを使用する場合 document.title;
+let shareUrl = location.href; // 現在のページURLを使用する場合 location.href;
+let shareText = title+'\n#駆け出しエンジニアと繋がりたい\n#プログラミング初心者'; // 現在のページタイトルを使用する場合 document.title;
 
 document.addEventListener('DOMContentLoaded', function () {
    
@@ -90,31 +90,31 @@ generate_share_button(snsArea, shareUrl, shareText,title);
 // シェアボタンを生成する関数
 function generate_share_button(area, url, text,title) {
     // シェアボタンの作成
-    var twBtn = document.createElement('div');
+    let twBtn = document.createElement('div');
     twBtn.className = 'twitter-btn';
-    var fbBtn = document.createElement('div');
+    let fbBtn = document.createElement('div');
     fbBtn.className = 'facebook-btn';
-    var liBtn = document.createElement('div');
+    let liBtn = document.createElement('div');
     liBtn.className = 'line-btn';
  
     // 各シェアボタンのリンク先
-    var twHref = 'https://twitter.com/share?text='+encodeURIComponent(text)+'&url='+encodeURIComponent(url);
-    var fbHref = 'http://www.facebook.com/share.php?u='+encodeURIComponent(url);
-    var liHref = 'https://line.me/R/msg/text/?'+encodeURIComponent(title)+' '+encodeURIComponent(url);
+    let twHref = 'https://twitter.com/share?text='+encodeURIComponent(text)+'&url='+encodeURIComponent(url);
+    let fbHref = 'http://www.facebook.com/share.php?u='+encodeURIComponent(url);
+    let liHref = 'https://line.me/R/msg/text/?'+encodeURIComponent(title)+' '+encodeURIComponent(url);
  
     // シェアボタンにリンクを追加
-    var clickEv = 'onclick="popupWindow(this.href); return false;"';
-    var twLink = '<a href="' + twHref + '" ' + clickEv + ' class = "twitter"><img src="/static/public/twitter.png" ></a>';
-    var fbLink = '<a href="' + fbHref + '" ' + clickEv + ' class = "facebook"><img src="/static/public/facebook.png" ></a>';
-    var liLink = '<a href="' + liHref + '" target="_blank" class = "line"><img src="/static/public/line.png" ></a>';
+    let clickEv = 'onclick="popupWindow(this.href); return false;"';
+    let twLink = '<a href="' + twHref + '" ' + clickEv + ' class = "twitter"><img src="/static/public/twitter.png" ></a>';
+    // var fbLink = '<a href="' + fbHref + '" ' + clickEv + ' class = "facebook"><img src="/static/public/facebook.png" ></a>';
+    // var liLink = '<a href="' + liHref + '" target="_blank" class = "line"><img src="/static/public/line.png" ></a>';
     twBtn.innerHTML = twLink;
-    fbBtn.innerHTML = fbLink;
-    liBtn.innerHTML = liLink;
+    // fbBtn.innerHTML = fbLink;
+    // liBtn.innerHTML = liLink;
  
     // シェアボタンを表示
     area.appendChild(twBtn);
-    area.appendChild(fbBtn);
-    area.appendChild(liBtn);
+    // area.appendChild(fbBtn);
+    // area.appendChild(liBtn);
 }
 
 const smoothScroll = () =>{
